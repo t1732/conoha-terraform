f047a93d4c4c9a687824fe3c9653e8dbf6b13834
t1732 <oo1732oo@gmail.com>
2021-02-18 20:59:04 +0900

    update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 }
 
 provider "openstack" {
-  user_name   = var.USER_NAME
-  password    = var.PASSWORD
-  tenant_name = var.TENANT_NAME
+  user_name   = var.user_name
+  password    = var.password
+  tenant_id = var.tenant_id
   auth_url    = var.auth_url
 }
 
 resource "openstack_compute_keypair_v2" "keypair" {
   name       = "terraform-conoha-vps"
-  public_key = var.PUBLIC_KEY
+  public_key = var.public_key
 }
